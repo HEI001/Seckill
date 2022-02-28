e6c78af865064e4eeb4920683bc2ba423df2f131
crystral <1023712219@qq.com>
2022-02-28 12:47:55 +0800

    两次MD5加密

---
+++ src/main/java/com/hei001/seckill/utils/MD5Utils.java
@@ -0,0 +1,51 @@
+package com.hei001.seckill.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author HEI001
+ * @date 2022/2/28 12:44
+ */
+@Component
+public class MD5Utils {
+    public static String md5(String src){
+        return DigestUtils.md5Hex(src);
+    }
+
+    private static final String salt = "1a2b3c4d";
+
+    /**
+     * 第一次加密
+     * @param inputPass
+     * @return
+     */
+    public static String inputPassToFormPass(String inputPass){
+        //避免在网络传输被截取然后反推出密码，所以在md5加密前先打乱密码
+        String str = "" + salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+    /**
+     * 第二次加密
+     * @param formPass
+     * @param salt
+     * @return
+     */
+    public static String formPassToDBPass(String formPass, String salt) {
+        String str = ""+salt.charAt(0)+salt.charAt(2) + formPass +salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+
+    public static String inputPassToDbPass(String input, String saltDB){
+        String formPass = inputPassToFormPass(input);
+        return formPassToDBPass(formPass, saltDB);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassToFormPass("123456"));
+        System.out.println(formPassToDBPass("d3b1294a61a07da9b49b6e22b2cbd7f9","1a2b3c4d"));
+        System.out.println(inputPassToDbPass("123456","1a2b3c4d"));
+    }
+}
